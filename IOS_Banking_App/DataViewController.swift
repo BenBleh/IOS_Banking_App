d93cbc03730d839b7e9aa4b3bdc56c31fe151feb
BenBleh <benjaminmatthys@gmail.com>
2016-03-21 10:22:33 +1100

    First commit

---
+++ IOS_Banking_App/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  IOS_Banking_App
+//
+//  Created by Benjamin Matthys on 21/03/2016.
+//  Copyright © 2016 Benjamin Matthys and Adrian Wakefield. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
